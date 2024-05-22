65f8a3f11807fa517a465d4adb5d5e94f5ef3c2a
CristofherPirela <154286679+CristofherPirela@users.noreply.github.com>
2024-05-21 20:22:35 -0400

    first commit

---
+++ src/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ItemListContainer = () => {
+  return (
+    <div>
+     
+      <h2>Lista de Items</h2>
+    </div>
+  );
+};
+
+export default ItemListContainer;
---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartWidget from '../CartWidget';
+
+const Navbar = () => {
+  return (
+    <nav>
+     
+      <CartWidget />
+    </nav>
+  );
+};
+
+export default Navbar;
